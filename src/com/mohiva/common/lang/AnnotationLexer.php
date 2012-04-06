18080fa37175dcbe3d0bd9db68181581aa67bc89
Christian Kaps <ck-github@mohiva.com>
2012-04-06 14:01:55 +0200

    TokenStream instance needn't be injected into lexers, it will be created on tokenization now

--- src/com/mohiva/common/lang/AnnotationLexer.php
+++ src/com/mohiva/common/lang/AnnotationLexer.php
@@ -88,54 +88,29 @@ class AnnotationLexer {
 	);
 
 	/**
-	 * The token stream.
-	 *
-	 * @var \com\mohiva\common\parser\TokenStream
-	 */
-	private $stream = null;
-
-	/**
-	 * The class constructor.
-	 *
-	 * @param \com\mohiva\common\parser\TokenStream $stream The token stream to use for this lexer.
-	 */
-	public function __construct(TokenStream $stream) {
-
-		$this->stream = $stream;
-	}
-
-	/**
-	 * Return the token stream instance.
-	 *
-	 * @return \com\mohiva\common\parser\TokenStream The token stream to use for this lexer.
-	 */
-	public function getStream() {
-
-		return $this->stream;
-	}
-
-	/**
 	 * Tokenize the given input string and return the resulting token stream.
 	 *
 	 * @param string $input The string input to scan.
-	 * @return \com\mohiva\common\parser\TokenStream The resulting token stream.
+	 * @return TokenStream The resulting token stream.
 	 */
 	public function scan($input) {
 
-		$this->stream->flush();
-		$this->stream->setSource($input);
-		$this->tokenize($input);
-		$this->stream->rewind();
+		$stream = $this->tokenize($input);
+		$stream->rewind();
 
-		return $this->stream;
+		return $stream;
 	}
 
 	/**
 	 * Transform the input string into a token stream.
 	 *
 	 * @param string $input The string input to tokenize.
+	 * @return TokenStream The resulting token stream.
 	 */
 	private function tokenize($input) {
+
+		$stream = new TokenStream;
+		$stream->setSource($input);
 
 		$pattern = '/' . implode('|', $this->lexemes) . '/';
 		$flags = PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_OFFSET_CAPTURE;
@@ -161,11 +136,13 @@ class AnnotationLexer {
 				$code = self::T_NONE;
 			}
 
-			$this->stream->push(new AnnotationToken(
+			$stream->push(new AnnotationToken(
 				$code,
 				$match[0],
 				$match[1]
 			));
 		}
+
+		return $stream;
 	}
 }
